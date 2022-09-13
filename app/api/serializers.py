d3f47c97282c9c0832f0b4a935a9385134a47f45
Felipe Treis <felipetreis1@gmail.com>
2022-09-12 22:21:49 -0300

    Created Serializers class

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -1,5 +1,32 @@
+from app.models import Book, Category, PublishingCompany
 from rest_framework import serializers
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name',
+        )
+
+
+class PublishingCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PublishingCompany
+        fields = (
+            'id',
+            'name',
+        )
+
+
 class BookSerializer(serializers.ModelSerializer):
-    ...
+    class Meta:
+        model = Book
+        fields = (
+            'id',
+            'name',
+            'state',
+            'category',
+            'publishing_company',
+        )
