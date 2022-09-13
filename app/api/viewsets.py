166321e8d28730260ff53bd9d2f27d2b89134fc9
Felipe Treis <felipetreis1@gmail.com>
2022-09-12 22:23:07 -0300

    Created viewset to category and publishing_company

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,5 +1,6 @@
-from app.api.serializers import BookSerializer
-from app.models import Book
+from app.api.serializers import (BookSerializer, CategorySerializer,
+                                 PublishingCompanySerializer)
+from app.models import Book, Category, PublishingCompany
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 
@@ -8,7 +9,19 @@ class Pagination(PageNumberPagination):
     page_size = 10
 
 
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = Pagination
+
+
+class PublishingCompanyViewSet(viewsets.ModelViewSet):
+    queryset = PublishingCompany.objects.all()
+    serializer_class = PublishingCompanySerializer
+    pagination_class = Pagination
+
+
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book
+    queryset = Book.objects.filter(is_available=True)
     serializer_class = BookSerializer
     pagination_class = Pagination
